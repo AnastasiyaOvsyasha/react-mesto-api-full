692473b467b590052cba495e9dfdc677577c67d1
Anastasiya Fedotova <nastenaf@list.ru>
2022-09-24 18:49:53 +0300

    cookie

--- backend/app.js
+++ backend/app.js
@@ -14,30 +14,20 @@ const { createUser, login } = require('./controllers/users');
 const ErrorNotFound = require('./errors/ErrorNotFound');
 
 const { PORT = 3000 } = process.env; //
-
 const limiter = rateLimit({
   //
   windowMs: 15 * 60 * 1000,
   max: 1000,
 });
-
 const app = express();
-
 app.use(
   cors({
-    origin: [
-      'http://crazy.nomoredomains.sbs',
-      'https://crazy.nomoredomains.sbs',
-    ],
+    origin: 'https://crazy.nomoredomains.sbs',
     credentials: true,
   }),
 );
-
-app.use(cors());
-
 app.use(helmet());
 app.use(limiter);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -50,7 +40,6 @@ app.get('/crash-test', () => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-
 app.post(
   '/signin',
   celebrate({
@@ -76,40 +65,32 @@ app.post(
   }),
   createUser,
 );
-
 app.use(auth);
-
 app.use('/cards', require('./routes/cards'));
 app.use('/users', require('./routes/users'));
 
 app.use('*', (req, res, next) => {
   next(new ErrorNotFound('Страница не найдена'));
 });
-
 app.use(errors());
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
   res.status(statusCode).send({ message });
   next();
 });
-
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-
 // подключаемся к серверу mongo
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/mestodb', {
     useNewUrlParser: true,
   });
   await app.listen(PORT);
-
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 }
-
 main();
